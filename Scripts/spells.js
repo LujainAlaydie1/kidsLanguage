bed2ace791e559af63a9644115d9c92308643396
LujainAlaydie1 <96392905+LujainAlaydie1@users.noreply.github.com>
2022-04-06 23:32:04 +0300

    fixed it.
    hi

---
+++ Scripts/spells.js
@@ -0,0 +1,15 @@
+const words = ["Apple", "Banana", "Cat", "Dog", "Egg",
+              "Fish", "Goat", "Hat", "Ice Cream", "Jacket",
+               "Key", "Lion", "Monkey", "Nest", "Orange",
+               "Pizza", "Queen", "Rabbit", "Sun", "Tiger",
+               "Umbrella", "Van", "Wave", "X-ray", "Yarn",
+               "Zebra"];
+
+
+
+const option1 = document.getElementById("option1"),
+      option2 = document.getElementById("option2"),
+      option3 = document.getElementById("option3"),
+      audio = document.getElementById("wrong");
+
+function generate_question(){ 
